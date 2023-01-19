f2e6a1f887b1c518100f2da9883c1a50d70ae304
Ivan <64004982+ivanzhiv32@users.noreply.github.com>
2023-01-19 15:54:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParsingOfEducationalinstitutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
